941ed459d50906497d67162baa92c62106819b97
AzzurroHotels <alvinrustia@azzurrohotels.com>
2026-02-17 17:28:06 +1100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,8 +1,7 @@
-// supabase-config.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const SUPABASE_URL = "https://lnigcfboeqsafcqgysgz.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuaWdjZmJvZXFzYWZjcWd5c2d6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNjg3NzgsImV4cCI6MjA4Njg0NDc3OH0.ZapbiFcQKOQQjEsdIVI61QgUyCEbcZojBkjoXWL6DcI";
+export const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_KEY_HERE";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
